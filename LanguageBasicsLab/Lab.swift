1236fc8afc8b99d95ddee01a110462d8889b5283
Kevin McQuown <kevin@thewcl.com>
2021-01-29 06:33:03 -0600

    Removed Solutions

--- LanguageBasicsLab/Lab.swift
+++ LanguageBasicsLab/Lab.swift
@@ -11,7 +11,7 @@ struct Lab {
     func helloWorld() -> String {
         var value : String
         // ----  Your code here ----
-        value = "Hello World!"        // <--- Fix this syntax so it compiles
+        value = Hello World!      // <--- Fix this syntax so it compiles
         // -------------------------
         return value
     }
@@ -21,9 +21,9 @@ struct Lab {
     // the value of the calculcation.  Replace the 0 in the return statement with this new variable.
     func add(a: Int, b: Int) -> Int {
         // ----  Your code here ----
-        let result = a + b
+
         // -------------------------
-        return result    // <--- Replace the 0 with your result
+        return 0    // <--- Replace the 0 with your result
     }
 
     // Computer the perimeter of a rectangle.  The variables width and height are
@@ -31,9 +31,9 @@ struct Lab {
     // the value of the calculcation.  Replace the 0 in the return statement with this new variable.
     func permimeterOfRectangle(width: Int, height: Int) -> Int {
         // ----  Your code here ----
-        let result = width * 2 + height * 2
+
         // -------------------------
-        return result    // <--- Replace the 0 with your result
+        return 0    // <--- Replace the 0 with your result
     }
 
     // Computer the area of a circle with a give radius.  The variable radius is already
@@ -41,22 +41,15 @@ struct Lab {
     // figure out if Swift already defines "pi" for you.  If so use that, otherwise define your own.
     func areaOfCircle(radius: Double) -> Double {
         // ----  Your code here ----
-        let result = Double.pi * pow(radius, 2)
+
         // -------------------------
-        return result    // <--- Replace the 0 with your result
+        return 0    // <--- Replace the 0 with your result
     }
     
     // Your are given two number.  Return the one which is larger. If they are equal, return 0
     func larger(a: Int, b: Int) -> Int {
         // ----  Your code here ----
-        if a > b {
-            return a
-        }
-        if a == b {
-            return 0
-        } else {
-            return b
-        }
+
         // -------------------------
     }
     
@@ -64,7 +57,7 @@ struct Lab {
     // For example, the following two numbers, 3.2 and 3.15 if the precision is 0.1 but not equal if the precision was 0.01
     func doublesAreEqual(a: Double, b: Double, precision: Double) -> Bool {
         // ----  Your code here ----
-        return abs(a - b) <= precision
+
         // -------------------------
     }
 }
